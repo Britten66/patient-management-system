d81256e8fe0252b1710de4737a61fe175d2aa530
Chris <98862485+Britten66@users.noreply.github.com>
2026-07-05 11:11:56 -0300

    added patient records code

---
+++ src/PatientRecord.java
@@ -0,0 +1,50 @@
+package com.project.patient;
+
+import java.time.LocalDate;
+
+public class PatientRecord {
+    private LocalDate visitDate;
+    private String diagnosis;
+    private String treatmentNotes;
+
+    public PatientRecord() {
+    }
+
+    public PatientRecord(LocalDate visitDate, String diagnosis, String treatmentNotes) {
+        this.visitDate = visitDate;
+        this.diagnosis = diagnosis;
+        this.treatmentNotes = treatmentNotes;
+    }
+
+    public LocalDate getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(LocalDate visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getTreatmentNotes() {
+        return treatmentNotes;
+    }
+
+    public void setTreatmentNotes(String treatmentNotes) {
+        this.treatmentNotes = treatmentNotes;
+    }
+
+    @Override
+    public String toString() {
+        return "PatientRecord - " +
+                "Visit Date: " + visitDate +
+                ", Diagnosis: " + diagnosis + '\'' +
+                ", Treatment Notes: " + treatmentNotes + '\'';
+    }
+}
